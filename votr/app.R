8652e1a2d4c9cd01212f4f1326d032d3370b2b95
Clau Dermont <clau.dermont@protonmail.ch>
2017-11-06 12:57:45 +0100

    update

--- votr/app.R
+++ votr/app.R
@@ -3,11 +3,14 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 library(ggswissmaps)
 
-data <- read.table("www/votedata.csv", sep=";", header=T, stringsAsFactors = F, fileEncoding = "UTF-8")
+data <- read.table("www/votedata.csv", sep=";", 
+                   header=T, stringsAsFactors = F, fileEncoding = "UTF-8")
 data[,c("suffrage","Stimmbeteiligung","Ja")] <- 
-  lapply(c("suffrage","Stimmbeteiligung","Ja"), function(x) as.numeric(data[,x]))
+  lapply(c("suffrage","Stimmbeteiligung","Ja"), 
+         function(x) as.numeric(data[,x]))
 data$vote <- factor(data$vote, as.character(unique(data$vote)))
 
 ui <- fluidPage(
@@ -23,8 +26,12 @@ ui <- fluidPage(
                       selectizeInput("selection", 
                                      label = h4("Abstimmung"), 
                                      choices = levels(data$vote), 
-                                     selected = "24.09.2017 Reform der Altersvorsorge 2020", 
+                                     selected = "21.05.2017 Energiegesetz", 
                                      multiple = FALSE), 
+                      div(
+                        style = "position:relative", 
+                        tableOutput("tbl")
+                      ), 
                       HTML("<b><a href='http://claudermont.ch'>claudermont.ch</a></br>
                            Datenquellen:</b> 
                            <a href='https://bfs.admin.ch/'>
@@ -33,7 +40,7 @@ ui <- fluidPage(
                column(9, 
                       div(
                         style = "position:relative",
-                        plotOutput("map", width="600px", height = "600px")
+                        plotOutput("map", width="600px", height = "450px")
                       ),
                       HTML("</br>"),
                       div(
@@ -52,12 +59,12 @@ ui <- fluidPage(
                       selectizeInput("selectionx", 
                                      label = h4("Abstimmung (X-Achse)"), 
                                      choices = levels(data$vote),
-                                     selected = "24.09.2017 Reform der Altersvorsorge 2020", 
+                                     selected = "21.05.2017 Energiegesetz", 
                                      multiple = FALSE), 
                       selectizeInput("selectiony", 
                                      label = h4("Abstimmung (Y-Achse)"), 
                                      choices = levels(data$vote), 
-                                     selected = "24.09.2017 Zusatzfinanzierung AHV über MwSt.", 
+                                     selected = "12.02.2017 Unternehmenssteuerreform III", 
                                      multiple = FALSE,
                                      options = NULL), 
                       radioButtons("key", 
@@ -88,15 +95,21 @@ server <- function(input, output){
   
   getData <- reactive({
     d <- data[which(data$vote %in% input$selection),]
+    d <- d[which(d$canton != "Schweiz"),]
   })
 
   getDataCompare <- reactive({
     d <- data[which(data$vote %in% c(input$selectionx, input$selectiony)),]
+    d <- d[which(d$canton != "Schweiz"),]
     d$vote <- as.character(d$vote)
     d$vote[d$vote==input$selectionx] <- "x"
     d$vote[d$vote==input$selectiony] <- "y"
-    d <- reshape(d, direction="wide", timevar="vote", idvar="canton", 
-                 v.names=c("Stimmbeteiligung","Ja","suffrage"))
+    d <- d[c("vote","canton","suffrage","Ja",
+             "Stimmbeteiligung","ling","nr")]
+    d <- d %>% 
+      gather(key, value, -ling, -nr, -canton, -vote) %>% 
+      unite(temp, key, vote, sep=".") %>% 
+      spread(temp, value)
     names(d) <- gsub(paste0(input$key,"."), "", names(d), fixed = TRUE)
     d <- d
     })
@@ -104,7 +117,20 @@ server <- function(input, output){
   getMap <- reactive({
     chmap <- shp_df[["g1k15"]]
     d <- data[which(data$vote %in% input$selection),]
+    d <- d[which(d$canton != "Schweiz"),]
     mapdata <- full_join(chmap, d, by=c("KTNR"="nr"))
+    mapdata$japrctcat <- cut(mapdata$Ja, 
+                             breaks=c(0, 0.3, 0.4, 0.45, 0.5,
+                                      0.55, 0.60, 0.7, 1), 
+                             labels=c("<30%", "30-40%", "40-45%", 
+                                      "45-50%", "50-55%", "55-60%", 
+                                      "60-70%", ">70%"))
+    mapdata <- mapdata
+  })
+  
+  getTable <- reactive({
+    tabl <- data[which(data$vote %in% input$selection &
+                         data$canton=="Schweiz"),]
   })
   
   # Tab 1
@@ -120,7 +146,7 @@ server <- function(input, output){
       scale_fill_brewer(palette="Set1") +
       labs(title=paste("Kantonsresultate: ", input$selection, sep=""),
            subtitle="Darstellung: Clau Dermont",
-           y="Stimmbeteiligung", 
+           y="Stimmbeteiligung in %", 
            x="Ja in %") +
       theme_minimal() +
       theme(legend.position="none",
@@ -132,13 +158,23 @@ server <- function(input, output){
     
     mapdata <- getMap()
     
-    p <- ggplot(mapdata, aes(x = long, y = lat, group = group, fill=Ja)) +
+    cols <- c("<30%" = "#b2182b", 
+              "30-40%" = "#d6604d", 
+              "40-45%" = "#f4a582", 
+              "45-50%" = "#fddbc7", 
+              "50-55%" = "#d1e5f0", 
+              "55-60%" = "#92c5de", 
+              "60-70%" = "#4393c3", 
+              ">70%" = "#2166ac")
+    
+    p <- ggplot(mapdata, aes(x = long, y = lat, group = group, fill=japrctcat)) +
       geom_polygon(color="white") +
       coord_equal() +
       labs(title=paste("Kantonsresultate: ", input$selection, sep=""),
            subtitle="Darstellung: Clau Dermont",
            fill="Ja in %", y="", x="") +
-      scale_fill_gradient2(low="#CC0000", mid="grey95", high="#0066CC", midpoint = 0.5) +
+      scale_fill_manual(values = cols,
+                        na.value = "grey50") +
       theme_minimal() +
       theme(panel.grid.major=element_blank(), 
             panel.grid.minor=element_blank(),
@@ -146,6 +182,18 @@ server <- function(input, output){
             text=element_text(size=15))
     p
     
+  })
+  
+  output$tbl <- renderTable({
+    tabl <- getTable()
+    tblmtrx <- matrix(ncol=2, nrow=5)
+    tblmtrx[,1] <- c("Stimmbeteiligung in %", "Ja-Stimmen", "Nein-Stimmen", 
+                     "Ja in %", "Stände-Ja")
+    tblmtrx[,2] <- c(tabl$Stimmbeteiligung*100, tabl$Ja.Stimmen, 
+                     tabl$Nein.Stimmen, tabl$Ja*100, tabl$staendeja)
+    tblmtrx <- as.data.frame(tblmtrx)
+    colnames(tblmtrx) <- c("Info", "Schweiz")
+    tblmtrx
   })
   
   #Tab 2
--- votr/recode_bfsdata.R
+++ votr/recode_bfsdata.R
@@ -1,19 +1,20 @@
 library(tidyverse)
 library(shiny)
 library(ggswissmaps)
+library(lubridate)
 
 
 data <- read.table("www/votedata_new.csv", sep=";", header=T, stringsAsFactors = F)
 
-data[,c("suffrage","Stimmbeteiligung","Ja", "Nein", "japrct", "valid", "Abgegebene.Stimmen")] <- 
-  lapply(c("suffrage","Stimmbeteiligung","Ja", "Nein", "japrct", "valid", "Abgegebene.Stimmen"), 
+data[,c("suffrage","Stimmbeteiligung","Ja.Stimmen", "Nein.Stimmen", "Ja", "valid", "Abgegebene.Stimmen")] <- 
+  lapply(c("suffrage","Stimmbeteiligung","Ja.Stimmen", "Nein.Stimmen", "Ja", "valid", "Abgegebene.Stimmen"), 
          function(x) as.numeric(data[,x]))
 
-data$japrct <- data$japrct/100
+data$Ja <- data$Ja/100
 data$Stimmbeteiligung <- data$Stimmbeteiligung/100
 
-data$result <- ifelse(data$Ja>data$Nein, "Ja", "Nein")
-data$resultnum <- ifelse(data$Ja>data$Nein, 1, 0)
+data$result <- ifelse(data$Ja.Stimmen>data$Nein.Stimmen, "Ja", "Nein")
+data$resultnum <- ifelse(data$Ja.Stimmen>data$Nein.Stimmen, 1, 0)
 
 data$halb <- 1
 data$halb[data$canton=="Appenzell Ausserrhoden" |
@@ -25,7 +26,6 @@ data$halb[data$canton=="Appenzell Ausserrhoden" |
 
 data$resultnum <- data$resultnum*data$halb
 
-  
 data <- data %>% 
   filter(canton!="Schweiz") %>% 
   select(vote, canton, resultnum) %>% 
@@ -34,5 +34,17 @@ data <- data %>%
   summarise(staendeja = sum(resultnum)) %>% 
   full_join(data, by="vote")
 
+# context
+context <- read.table("www/context.csv", sep=";", header=T, stringsAsFactors = F)
+data <- full_join(data, context, by="canton")
+
+# sort
+data$datum <- dmy(substr(data$vote, 1, 10))
+data <- arrange(data, desc(datum))
+
+data <- arrange(data, canton, desc(datum))
+data <- fill(data, suffrage)
+
+data <- arrange(data, desc(datum))
 
 write.table(data, "www/votedata.csv", sep=";", row.names = F, fileEncoding = "UTF-8")
